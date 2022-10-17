2f70a551210b76e3f2672622d97ed7b2044bd1c9
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-10-17 17:53:36 +0700

    Exception layer added

---
+++ customer-service/src/main/java/ru/tumasoff/micro/customerservice/listener/CustomerListener.java
@@ -0,0 +1,24 @@
+package ru.tumasoff.micro.customerservice.listener;
+
+//import org.bson.types.ObjectId;
+//import org.mkzaman.microservice.customer.domain.Customer;
+//import org.springframework.data.mongodb.core.mapping.event.BeforeConvertCallback;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Objects;
+//
+// TODO: Is this really need?
+//@Component
+//public class CustomerListener implements BeforeConvertCallback<Customer> {
+//
+//    @Override
+//    public Customer onBeforeConvert(Customer entity, String collection) {
+//        System.out.println("Customer Name = " + entity.getLastName());
+//        if (Objects.isNull(entity.getBillingAddress().getId())) {
+//            ObjectId id = new ObjectId();
+//            String x = id.toString();
+//            entity.getBillingAddress().setId(id.toString());
+//        }
+//        return entity;
+//    }
+//}
